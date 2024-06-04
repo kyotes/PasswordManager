687c08b2b85b40191b1746fd47c4aa22b6dc7903
kyo <kyo.testarossa@gmail.com>
2024-06-05 04:04:33 +0900

    パスワードマネージャー

--- password_manager.sh
+++ password_manager.sh
@@ -1,12 +1,23 @@
 #!/bin/bash
-
 echo 'パスワードマネージャーへようこそ！'
-echo '次の選択肢から入力してください(Add Password/Get Password/Exit):'
 
-read key
+while True
+do
 
-if [ "$key" = "Add Password" ]; then
-    echo pass
-else
-    echo error
-fi
+    echo -n '次の選択肢から入力してください(Add Password/Get Password/Exit):'
+    read key
+    echo
+
+    if [ "$key" = "Add Password" ]; then
+        source Addpassword.sh
+    elif [ "$key" = "Get Password" ]; then
+        source Getpassword.sh
+    elif [ "$key" = "Exit" ]; then
+        echo 'Thank you!'
+        break
+    else
+        echo "入力が間違えています。Add Password/Get Password/Exit から入力してください。"
+        echo
+    fi
+
+done
