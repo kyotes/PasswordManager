95345b99274ec247d57f7fb00fd9faba2bff2147
kyo <kyo.testarossa@gmail.com>
2024-06-05 02:51:37 +0900

    改行、出力文字の調整

--- Addpass.sh
+++ Addpass.sh
@@ -2,14 +2,14 @@
 
 echo 'パスワードマネージャーへようこそ！'
 
-echo 'サービス名を入力してください：'
+echo -n 'サービス名を入力してください：'
 read subname
 echo 'サービス名：'$subname >> pass.txt
-echo 'ユーザー名を入力してください：'
+echo -n 'ユーザー名を入力してください：'
 read username
 echo 'ユーザー名：'$username >> pass.txt
-echo 'パスワードを入力してください：'
+echo -n 'パスワードを入力してください：'
 read pass
 echo 'パスワード：'$pass >> pass.txt
 
-echo 'Thank you!'
+echo 'パスワードの追加は成功しました。'
